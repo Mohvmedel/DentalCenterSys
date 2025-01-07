b26fd6eb6f69508df583a35f3b9b3f3d4bc7a341
Mohvmedel <mohvmedelkhvlfi@gmail.com>
2025-01-08 00:03:00 +0100

    Revert "Commit message for selected microservices"
    This reverts commit a9ba123389e510377f4107fd4d6aa12f5f5fca4b.

--- patient-service/src/main/java/com/dentalcenter/patient/entity/Gender.java
+++
@@ -1,7 +0,0 @@
-package com.dentalcenter.patient.entity;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    OTHER
-}
--- patient-service/src/main/java/com/dentalcenter/patient/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.dentalcenter.patient.exception;
-
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
